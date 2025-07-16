82a8bd297f3be79e8afe9d9cbc0d684af1377e3f
Luci0 <luci00@gmail.com>
2025-07-16 15:47:37 +0300

    commit history

--- plugin/remaps.lua
+++ plugin/remaps.lua
@@ -3,3 +3,7 @@ vim.o.winbar = '%=%f'
 vim.keymap.set('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>gi', { noremap = true, silent = true })
 vim.keymap.set('v', '<C-s>', '<Esc>:w<CR>gv', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>hk', function()
+  require('telescope.builtin').git_commits()
+end, { desc = 'List Git commits in current branch' })
